271564dfb7971306756007b6a3fbac421dd238e1
CodyConck <codyconck@gmail.com>
2021-09-08 17:00:07 -0500

    updated server, auth, resolvers, typeDefs, queries, mutations'

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me(username: String, _id: String): User
+}
+
+type Mutation {
+    login(email: String, username: String, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, bookId: String!, title: String!, image: String, link: String): User
+    removeBook(bookId: String!): User
+}
+    
+`;
+
+module.exports = typeDefs;
