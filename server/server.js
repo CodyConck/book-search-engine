2d52900c5b566a58e663bbbe144739ff78aacb7f
CodyConck <codyconck@gmail.com>
2021-09-12 16:47:21 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth')
@@ -13,6 +13,7 @@ const server = new ApolloServer({
   typeDefs, 
   resolvers,
   context: authMiddleware
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
 })
 
 //integrate Apollo w/ Express applications as middleware
@@ -28,8 +29,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
